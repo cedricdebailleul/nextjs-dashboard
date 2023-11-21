bc468c9c2dcd8eb58ac779f07146c1a8850b4b47
cedric debailleul <cedric.debailleul62@gmail.com>
2023-11-21 23:38:18 +0100

    ajout auth

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,11 +4,21 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { useSearchParams, usePathname } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
 
+  const currentPage = Number(useSearchParams().get('page')) || 1;
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   // const allPages = generatePagination(currentPage, totalPages);
+
+  const createPageURL = (page: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    return `${pathname}?${params.toString()}`;
+  }
 
   return (
     <>
